5a9851cd2e5d1d38fb2d6bf2f0075ddeb547a314
andreasiposova <30650191+andreasiposova@users.noreply.github.com>
2023-01-16 00:17:01 +0100

    rf sensitivity visualization done

--- rf_hyperparams.py
+++ rf_hyperparams.py
@@ -11,6 +11,7 @@ from sklearn.metrics import make_scorer, roc_auc_score, accuracy_score, precisio
 from sklearn.model_selection import GridSearchCV
 from datetime import datetime
 import matplotlib.pyplot as plt
+from visualization import sensitivity_plot, get_scores
 
 from skopt import BayesSearchCV
 from skopt.space import Integer, Categorical
@@ -90,25 +91,6 @@ def grid_search_rf(X_train, X_test, y_train, y_test, file_name):
                             'recall': recall, 'roc_auc': roc_auc, 'f1': f1, 'f2': f2})
     return test_scores
 
-"""
-    # Define the scoring methods
-    scoring = {
-        'recall': make_scorer(recall_score),
-        'accuracy': make_scorer(accuracy_score),
-        'precision': make_scorer(precision_score),
-        'roc_auc': make_scorer(roc_auc_score),
-        'f1': make_scorer(f1_score),
-        'f2': make_scorer(fbeta_score, beta=2)
-    }
-
-    # Instantiate the classifier
-    rf = RandomForestClassifier()
-
-    # Perform the grid search
-    grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, cv=10, scoring=scoring, refit='recall',
-                               return_train_score=True)
-    grid_search.fit(X_train, y_train)
-
     # Get the best estimator
     best_estimator = grid_search.best_estimator_
 
@@ -124,18 +106,16 @@ def grid_search_rf(X_train, X_test, y_train, y_test, file_name):
     # Return the results
     return {'accuracy': accuracy, 'precision': precision, 'recall': recall, 'roc_auc': roc_auc, 'f1': f1, 'f2': f2}
 
-"""
 
 
 def visualize_results(test_scores, file_name):
     # Create a folder named "plots" if it doesn't exist
     if not os.path.exists('plots'):
         os.makedirs('plots')
+    if not os.path.exists('plots/' + str(file_name)):
+        os.makedirs('plots/' + str(file_name))
 
-    # Extract the accuracy values
-    accuracy_values = [score['accuracy'] for score in test_scores]
-    recall_values = [score['recall'] for score in test_scores]
-    roc_auc_score = [score['roc_auc'] for score in test_scores]
+    scores = get_scores(test_scores)
 
     # Extract the parameter values
     param_values = [score['params'] for score in test_scores]
@@ -145,17 +125,14 @@ def visualize_results(test_scores, file_name):
     min_samples_leaf = [param['min_samples_leaf'] for param in param_values]
     max_features = [param['max_features'] for param in param_values]
 
-    # Plot the relationship between accuracy and n_estimators
-    plt.plot(n_estimators, accuracy_values,label='Accuracy')
-    plt.plot(n_estimators, recall_values,label='Recall')
-    plt.plot(n_estimators, roc_auc_score, label='ROC AUC Score')
-    plt.xlabel('n_estimators')
-    plt.ylabel('Score')
-    plt.legend()
-    plt.title(file_name)
+    sensitivity_plot(n_estimators, scores, file_name, "n_estimators")
+    sensitivity_plot(max_depth, scores, file_name, "max_depth")
+    sensitivity_plot(min_samples_split, scores, file_name, "min_samples_split")
+    sensitivity_plot(min_samples_leaf, scores, file_name, "min_samples_leaf")
+    sensitivity_plot(max_features, scores, file_name, "max_features")
 
-    plt.savefig(os.path.join(Config.PLOTS_DIR, f"{file_name}_rf_n_estimators_sensitivity.png"))
-    plt.show()
+    # Plot the relationship between accuracy and n_estimators
+
     #plt.savefig('images/' + file_name)
 
 
---
+++ visualization.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import os
+from utils import Config
+
+def get_scores(test_scores):
+    scores = []
+    # Extract the accuracy values
+    accuracy_values = [score['accuracy'] for score in test_scores]
+    precision_values = [score['precision'] for score in test_scores]
+    recall_values = [score['recall'] for score in test_scores]
+    roc_auc_values = [score['roc_auc'] for score in test_scores]
+    f1_values =  [score['f1'] for score in test_scores]
+    f2_values =  [score['f2'] for score in test_scores]
+    scores.append(accuracy_values)
+    scores.append(precision_values)
+    scores.append(recall_values)
+    scores.append(roc_auc_values)
+    scores.append(f1_values)
+    scores.append(f2_values)
+    return scores
+
+def sensitivity_plot(metric, scores, file_name, xlabel):
+    accuracy_values = scores[0]
+    precision_values = scores[1]
+    recall_values = scores[2]
+    roc_auc_values = scores[3]
+    f1_values = scores[4]
+    f2_values = scores[5]
+
+
+    plt.plot(metric, accuracy_values, label='Accuracy')
+    plt.plot(metric, precision_values, label='Precision')
+    plt.plot(metric, recall_values, label='Recall')
+    plt.plot(metric, roc_auc_values, label='ROC AUC Score')
+    plt.plot(metric, f1_values, label='F1 Score')
+    plt.plot(metric, f2_values, label='F2 Score')
+    plt.xlabel(xlabel)
+    plt.ylabel('Score')
+    plt.legend()
+    plt.title(file_name)
+    plt.savefig(os.path.join(Config.PLOTS_DIR, file_name, f"rf_n_estimators_sensitivity.png"))
+    plt.show()
