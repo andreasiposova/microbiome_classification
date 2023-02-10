3cd15dc4ab943eed8403318e942912952829f3e5
andreasiposova <siposova.andrea@gmail.com>
2023-02-10 02:24:24 +0100

    model comparison

--- preprocessing.py
+++ preprocessing.py
@@ -33,10 +33,13 @@ def preprocess_data(dataset, metadata_path):#file_name, metadata_path):
     X = data.iloc[:, :-1]
     y = data.iloc[:, -1]
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1234)
-
+    #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1234)
+    X_train = X
+    y_train = y
+    X_test = pd.DataFrame()
+    y_test = pd.DataFrame()
     X_train, y_train = replace_inf_vals(X_train, y_train)
-    X_test, y_test =replace_inf_vals(X_test, y_test)
+    #X_test, y_test =replace_inf_vals(X_test, y_test)
 
     #labelEncoder
     #le = preprocessing.LabelEncoder()
@@ -47,7 +50,7 @@ def preprocess_data(dataset, metadata_path):#file_name, metadata_path):
 
     label_mapping = {'healthy': 0, 'CRC': 1}
     y_train = [label_mapping[label] for label in y_train]
-    y_test = [label_mapping[label] for label in y_test]
+    #y_test = [label_mapping[label] for label in y_test]
 
     return X_train, X_test, y_train, y_test
 
